37fa2c9645d688122a7dc8a15f194e6146c3d017
Rodfc773 <rodrigo.fc773@gmail.com>
2023-04-26 10:35:32 -0300

    Corrigindo a criacao do grafo em C e terminando a implementacao da DFS em C

--- Monitoria_2023/BuscasEmGrafos/DFS/DFSinC.c
+++ Monitoria_2023/BuscasEmGrafos/DFS/DFSinC.c
@@ -3,7 +3,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void DFS_initial(struct Graph G, int V){
+void DFS(int v, struct Graph *G, struct Stack *stack , int visitados[]){
+
+    visitados[v] = 1;
+
+    struct Node* vertice = G->head[v];
+
+    printf("Estamos no vértice: %d e vamos para o vertice: %d\n", v, vertice->dest);
+    while (vertice != NULL
+    )
+    {
+        if(!(visitados[vertice->dest])){
+
+            stack = push(stack,vertice->dest);
+
+            DFS(vertice->dest, G, stack, visitados);
+
+            stack = pop(stack);
+        }
+
+        vertice = vertice->next;
+    }
+    
+    
+
+}
+void DFS_initial(struct Graph *G, int V){
 
     int visitados[V];
 
@@ -12,29 +37,31 @@ void DFS_initial(struct Graph G, int V){
         visitados[i] = 0;
     }
 
-    int vo = 0;
+    int v = 0;
 
     struct Stack *stack = NULL;
 
-    stack = push(stack, vo);
+    stack = push(stack, v);
 
-    
+    DFS(v, G, stack, &visitados[0]);
 
+    stack = pop(stack);
 
-    
 }
 int main(){
 
 
     struct Edge edges[]={
 
-        {0, 1}, {1, 2}, {2, 0}, {2, 1}, {3, 2}, {4, 5}, {5, 4}, {7,2}, {3,0}, {5,2}
+        {0, 1}, {1, 2}, {2, 0}, {2, 1}, {3, 2}, {4, 5}, {5, 4}, {3, 5}
     };
     
     int V = sizeof(edges)/sizeof(edges[0]);
 
     struct Graph *graph = createGraph(edges, V);
     printGraph(graph);
+
+    DFS_initial(graph, V);
 
     return 0;
 }
